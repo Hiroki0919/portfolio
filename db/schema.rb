44a55c1b237fd376bf43ea1b80e11bde3e2b59ee
Hiroki0919 <hiroki249685@ezweb.ne.jp>
2020-08-03 22:11:30 +0900

    db:migrateのし忘れコミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_074757) do
+ActiveRecord::Schema.define(version: 2020_08_03_130435) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2020_08_03_074757) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
